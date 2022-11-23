3b67a59e286e83e4e138099a3403852d6b31a103
Joas Sahetapy <joassahetapy@gmail.com>
2022-11-23 21:06:09 +0100

    Added new classes and header files and changed square.tga

--- start/myscene.cpp
+++ start/myscene.cpp
@@ -9,6 +9,7 @@
 
 #include "myscene.h"
 
+
 MyScene::MyScene() : Scene()
 {
 	// start the timer.
@@ -16,22 +17,35 @@ MyScene::MyScene() : Scene()
 
 	// create a single instance of MyEntity in the middle of the screen.
 	// the Sprite is added in Constructor of MyEntity.
-	myentity = new MyEntity();
-	myentity->position = Point2(SWIDTH/2, SHEIGHT/2);
+	player = new Player();
+	enemy = new MyEntity();
+	base = new MyEntity();
+	player->position = Point2(SWIDTH/3, SHEIGHT/2);
+	enemy->position = Point2(SWIDTH/4, SHEIGHT/4);
+	base->position = Point2(SWIDTH/2, SHEIGHT/2);
+	player->scale = Point2(1, 1);
+	enemy->scale = Point2(4, 1);
+	base->scale = Point2(0.9, 0.9);
 
 	// create the scene 'tree'
 	// add myentity to this Scene as a child.
-	this->addChild(myentity);
+	this->addChild(player);
+	this->addChild(enemy);
+	this->addChild(base);
 }
 
 
 MyScene::~MyScene()
 {
 	// deconstruct and delete the Tree
-	this->removeChild(myentity);
+	this->removeChild(player);
+	this->removeChild(enemy);
+	this->removeChild(base);
 
 	// delete myentity from the heap (there was a 'new' in the constructor)
-	delete myentity;
+	delete player;
+	delete enemy;
+	delete base;
 }
 
 void MyScene::update(float deltaTime)
@@ -45,45 +59,51 @@ void MyScene::update(float deltaTime)
 	// ###############################################################
 	// Camera Scaling??
 	// ###############################################################
-	if (input()->getKeyDown(KeyCode::O)) {
-		myentity->scale = Point(0.5f, 0.5f);
-	}if (input()->getKeyDown(KeyCode::P)) {
-		myentity->scale = Point(0.5f, 0.5f);
-	}
 
 
-
+	
 	// ###############################################################
 	// Spacebar scales myentity
 	// ###############################################################
-	if (input()->getKeyDown(KeyCode::Space)) {
-		myentity->scale = Point(0.5f, 0.5f);
-	}
-	if (input()->getKeyUp(KeyCode::Space)) {
-		myentity->scale = Point(1.0f, 1.0f);
-	}
+	
+	//if (input()->getKeyDown(KeyCode::Space)) {
+	//	player->scale = Point(0.5f, 0.5f);
+	//}
+	//if (input()->getKeyUp(KeyCode::Space)) {
+	//	player->scale = Point(1.0f, 1.0f);
+	//}
+	
 	// ###############################################################
 	// WASD moves the cube
 	// ###############################################################
 	if (input()->getKey(KeyCode::W)) {
-		myentity->position -= Point3(0, 2, 0);
+		player->position -= Point3(0, 1, 0);
 	}
 	if (input()->getKey(KeyCode::S)) {
-		myentity->position += Point3(0, 2, 0);
+		player->position += Point3(0, 1, 0);
 	}
 	if (input()->getKey(KeyCode::A)) {
-		myentity->position -= Point3(2, 0, 0);
+		player->position -= Point3(1, 0, 0);
 	}
 	if (input()->getKey(KeyCode::D)) {
-		myentity->position += Point3(2, 0, 0);
+		player->position += Point3(1, 0, 0);
 	}
+
+	float mx = input()->getMouseX();
+	float my = input()->getMouseY();
+
+	// clears previous Debug Draw calls
+	ddClear();
+
+	ddLine(player->position.x, player->position.y, mx, my, GREEN);
+	ddLine(base->position.x, base->position.y, mx, my, YELLOW);
 
 	// ###############################################################
 	// Rotate color
 	// ###############################################################
 	if (t.seconds() > 0.0333f) {
-		RGBAColor color = myentity->sprite()->color;
-		myentity->sprite()->color = Color::rotate(color, 0.01f);
+		RGBAColor color = player->sprite()->color;
+		player->sprite()->color = Color::rotate(color, 0.01f);
 		t.start();
 	}
 }
--- start/myscene.h
+++ start/myscene.h
@@ -12,6 +12,7 @@
 #include <rt2d/scene.h>
 
 #include "myentity.h"
+#include "player.h"
 
 /// @brief The MyScene class is the Scene implementation.
 class MyScene : public Scene
@@ -29,7 +30,9 @@ public:
 
 private:
 	/// @brief the rotating square in the middle of the screen
-	MyEntity* myentity;
+	Player* player;
+	MyEntity* enemy;
+	MyEntity* base;
 	/// @brief a Timer to rotate the color every n seconds
 	Timer t;
 };
